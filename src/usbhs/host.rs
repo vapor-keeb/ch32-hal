8b36f7ead60d571411e7353315ef92599999d666
Dummyc0m <xieyuanchu@gmail.com>
2024-12-03 22:54:42 -0800

    bus reset and get descriptor

--- src/usbhs/host.rs
+++ src/usbhs/host.rs
@@ -4,11 +4,13 @@ use core::{
     task::{Poll, Waker},
 };
 
-use crate::interrupt::typelevel::Interrupt;
+use crate::{interrupt::typelevel::Interrupt, usb::EndpointDataBuffer};
 use ch32_metapac::usbhs::vals::SpeedType;
 use defmt::Format;
 use embassy_hal_internal::Peripheral;
 use embassy_sync::waitqueue::AtomicWaker;
+use embassy_time::Timer;
+use embassy_usb_driver::EndpointError;
 
 use crate::{
     gpio::{AFType, Speed},
@@ -18,17 +20,55 @@ use crate::{
 use super::Instance;
 
 const MAX_PACKET_SIZE: usize = 64;
-static EP_TX_BUF: [u8; MAX_PACKET_SIZE] = [0; MAX_PACKET_SIZE];
-static EP_RX_BUF: [u8; MAX_PACKET_SIZE] = [0; MAX_PACKET_SIZE];
 
 static BUS_WAKER: AtomicWaker = AtomicWaker::new();
+static TX_WAKER: AtomicWaker = AtomicWaker::new();
+// static RX_WAKER: AtomicWaker = AtomicWaker::new();
 
 pub struct InterruptHandler<T: Instance> {
     _phantom: PhantomData<T>,
 }
 
-pub struct USBHsHostDriver<T: Instance> {
+pub struct USBHsHostDriver<'d, T: Instance> {
     _phantom: PhantomData<T>,
+    tx_buf: &'d mut EndpointDataBuffer,
+    rx_buf: &'d mut EndpointDataBuffer,
+}
+
+impl<'d, T: Instance> USBHsHostDriver<'d, T> {
+    pub async fn setup(&mut self, buf: &[u8]) -> Result<(), EndpointError> {
+        let h = T::hregs();
+
+        self.tx_buf.write_volatile(buf);
+        h.ep_pid().write(|v| {
+            v.set_endp(0);
+            v.set_token(0b1101);
+        });
+
+        h.tx_len().write(|v| v.set_len(buf.len() as u16));
+        h.rx_ctrl().write(|_| {});
+        h.tx_ctrl().write(|_| {});
+
+        assert!(!h.int_fg().read().transfer());
+
+        poll_fn(|ctx| {
+            TX_WAKER.register(ctx.waker());
+            let transfer = h.int_fg().read().transfer();
+            let status = h.int_st().read();
+
+            if transfer {
+                trace!("status h_res {}", status.0);
+                h.ep_pid().write(|_| {});
+                h.int_fg().write(|w| w.set_transfer(true));
+                critical_section::with(|_| h.int_en().modify(|w| w.set_transfer(true)));
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        })
+        .await;
+        Ok(())
+    }
 }
 
 pub struct Bus<T: Instance> {
@@ -44,6 +84,31 @@ pub enum Event {
 }
 
 impl<T: Instance> Bus<T> {
+    pub async fn reset(&mut self) {
+        // follow example code from openwch
+        T::hregs().dev_ad().write(|v| v.set_addr(0));
+        critical_section::with(|_| {
+            T::hregs().ctrl().modify(|v| {
+                v.set_tx_bus_reset(true);
+            });
+        });
+        Timer::after_millis(15).await;
+        critical_section::with(|_| {
+            T::hregs().ctrl().modify(|v| {
+                v.set_tx_bus_reset(false);
+            });
+        });
+        Timer::after_millis(2).await;
+        // copied from openwch
+        if T::hregs().int_fg().read().detect() {
+            if T::hregs().mis_st().read().dev_attach() {
+                panic!("attach after reset");
+                T::hregs().int_fg().write(|v| v.set_detect(true));
+                critical_section::with(|_| T::hregs().int_en().modify(|v| v.set_detect(true)));
+            }
+        }
+    }
+
     pub async fn poll(&mut self) -> Event {
         poll_fn(|ctx| {
             BUS_WAKER.register(ctx.waker());
@@ -63,6 +128,9 @@ impl<T: Instance> Bus<T> {
 
                 Poll::Ready(res)
             } else if flags.suspend() {
+                // small problem
+                // currently suspend fires after device attach
+                // unclear what this means
                 let res = if regs.mis_st().read().suspend() {
                     Event::Suspend
                 } else {
@@ -91,10 +159,15 @@ impl<T: Instance> interrupt::typelevel::Handler<T::Interrupt> for InterruptHandl
         if flag.detect() {
             r.int_en().modify(|v| v.set_detect(false));
             BUS_WAKER.wake();
-        } 
+        }
         if flag.suspend() {
             r.int_en().modify(|v| v.set_suspend(false));
             BUS_WAKER.wake();
+        }
+        if flag.transfer() {
+            r.int_en().modify(|v| v.set_transfer(false));
+            TX_WAKER.wake();
+            // RX_WAKER.wake();
         }
 
         trace!("irq: {:x}", flag.0);
@@ -104,7 +177,9 @@ impl<T: Instance> interrupt::typelevel::Handler<T::Interrupt> for InterruptHandl
 pub fn start<'d, T: Instance>(
     dp: impl Peripheral<P = impl super::DpPin<T, 0> + 'd>,
     dm: impl Peripheral<P = impl super::DmPin<T, 0> + 'd>,
-) -> (Bus<T>, USBHsHostDriver<T>) {
+    tx_buf: &'d mut EndpointDataBuffer,
+    rx_buf: &'d mut EndpointDataBuffer,
+) -> (Bus<T>, USBHsHostDriver<'d, T>) {
     let dp = dp.into_ref();
     let dm = dm.into_ref();
 
@@ -137,8 +212,8 @@ pub fn start<'d, T: Instance>(
         w.set_h_tx_en(true);
         w.set_h_rx_en(true);
     });
-    h.rx_dma().write_value((&EP_RX_BUF).as_ptr() as u32);
-    h.tx_dma().write_value((&EP_TX_BUF).as_ptr() as u32);
+    h.rx_dma().write_value(rx_buf.addr() as u32);
+    h.tx_dma().write_value(tx_buf.addr() as u32);
 
     h.ctrl().write(|w| w.set_phy_suspendm(true));
 
@@ -155,5 +230,12 @@ pub fn start<'d, T: Instance>(
 
     unsafe { T::Interrupt::enable() };
 
-    (Bus { _phantom: PhantomData }, USBHsHostDriver { _phantom: PhantomData })
+    (
+        Bus { _phantom: PhantomData },
+        USBHsHostDriver {
+            tx_buf,
+            rx_buf,
+            _phantom: PhantomData,
+        },
+    )
 }
